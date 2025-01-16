980615bcfbfa11839a7df15c9ee4fb01411dac38
roehrt <33495148+roehrt@users.noreply.github.com>
2025-01-16 18:44:32 +0100

    Konni

--- submission/main_tournament/src/main.cpp
+++ submission/main_tournament/src/main.cpp
@@ -39,21 +39,38 @@ struct Bot {
         auto rounds_left = NUM_ROUNDS - gameState->roundNum;
         auto duration = chrono::nanoseconds((long long)(1e9 * (gameState->gameClock-1) / rounds_left));
 
-        double foldEv = -roundState->pips[active];
-        double eq = equity(roundState->hands[active], roundState->deck, duration);
-        double callEv = eq * roundState->pips[1 - active] - (1 - eq) * roundState->pips[1 - active];
-        // This is dumb.
-        if (callEv - foldEv > 0.15*roundState->pips[1 - active]) {
-            double minRaiseEq = 0.9;
-            if (legalActions.contains(Action::Type::RAISE) && eq > minRaiseEq) {
-                auto [minRaise, maxRaise] = roundState->raiseBounds();
-                int raise = (eq - minRaiseEq) * (maxRaise - minRaise) + minRaise;
-                raise = clamp(raise, minRaise, maxRaise);
+#define pip(player) (100-roundState->stacks[player])
+        double foldEv = -pip(active);
+        double eq = equity(roundState->hands[active], roundState->deck, 3.0/4*duration);
+        auto [minRaise, maxRaise] = roundState->raiseBounds();
+        if (roundState->street == 0 && eq > 0.45) {
+            if (pip(active) < 5) {
+                int raise = clamp(5 - pip(active), minRaise, maxRaise);
+                return Action{Action::Type::RAISE, raise};
+            }
+        }
+        int opip = pip(1 - active);
+        if (roundState->street == 3) opip *= 3, opip /= 2;
+        if (roundState->street >= 3) opip *= 2;
+        opip = min(opip, 100);
+        eq -= 0.03;
+        double callEv = eq * opip - (1 - eq) * opip;
+        if (callEv - foldEv > 5 && roundState->street > 0) {
+            double threshold = 1.0;
+            if (roundState->street == 3) threshold = 0.7;
+            if (roundState->street == 4) threshold = 0.65;
+            if (roundState->street >= 5) threshold = 0.6;
+            int want = pow(max(0.0, (eq - threshold) / (1-threshold)), 2)*min(100, 2*pip(1 - active)); // Don't look for meaning, there is none.
+            if (legalActions.contains(Action::Type::RAISE) && pip(1 - active) < want && pip(active) + minRaise <= want) {
+                int raise = clamp(want - pip(active), minRaise, maxRaise);
+                for (auto card : roundState->deck) cerr << card << "";
+                cerr << ": " << roundState->hands[active][0] << roundState->hands[active][1] << " " << pip(active) << " -> " << pip(active) + raise << endl;
                 return Action{Action::Type::RAISE, raise};
             }
             return checkCall(legalActions);
         }
         if (legalActions.contains(Action::Type::CHECK)) return Action{Action::Type::CHECK};
+        cerr << pip(active) << " " << pip(1-active) << " folded" << endl;
         return Action{Action::Type::FOLD};
     }
 };
